e657c8b2536365f06489ae67fd1df1a780383bab
Franklin Thaker <franklin873@live.com>
2020-07-08 13:03:49 +0530

    client.html, package.json and server.js > broadcasting websocket demo for nodejs

---
+++ server.js
@@ -0,0 +1,40 @@
+/*
+const http = require('http');
+const WebSocketServer = require('websocket').server;
+const server = http.createServer();
+server.listen(9898);
+const wsServer = new WebSocketServer({
+    httpServer: server
+});
+wsServer.on('request', function(request) {
+    const connection = request.accept(null, request.origin);
+    connection.on('message', function(message) {
+      console.log('Received Message:', message.utf8Data);
+      connection.sendUTF('Hi this is WebSocket server!');
+    });
+    connection.on('close', function(reasonCode, description) {
+        console.log('Client has disconnected.');
+    });
+}); */
+
+const express = require('express');
+const http = require('http');
+const WebSocket = require('ws');
+
+const port = 3000;
+const server = http.createServer(express);
+const wss = new WebSocket.Server({ server })
+
+wss.on('connection', function connection(ws) {
+  ws.on('message', function incoming(data) {
+    wss.clients.forEach(function each(client) {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(data);
+      }
+    })
+  })
+})
+
+server.listen(port, function() {
+  console.log(`Server is listening on ${port}!`)
+})
